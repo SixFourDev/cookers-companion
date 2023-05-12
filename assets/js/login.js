7129eaac92bd5b93c62d0541df52108076f37427
Neta2393 <benetheahardin@yahoo.com>
2023-05-11 21:28:41 -0700

    Added the beginning stages of style element to login html

--- assets/js/login.js
+++ assets/js/login.js
@@ -3,16 +3,11 @@ function saveCredentials() {
     var password = document.getElementById("#password").value;
     var login = document.getElementById("#login").value;
     var data = {"username": username,"password": password, "login": login};
-    fetch('/save', {
+    fetch ({
         method: 'GET',
         body: JSON.stringify(data)
     })
     .then(response => response.json())
     .then(data => console.log(data))
     .catch(error => console.error(error));
-
-addEventListener('submit', login)
-}
-{
-    let loginbtn= document.getElementById("loginbtn");
 }
