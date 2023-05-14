c741d734e2a7bb4692e20187625ab6c203fcb138
SixFourDev <sb_94@yahoo.com>
2023-05-13 18:34:07 -0700

    completed displayRecipe function, and now displays recipes in top searches section

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,6 +29,37 @@ var recipeElements = [
       console.log(error);
     }
   }
+
+  // Creates displayRecipe function and takes recipe and recipeElement as two arguments
+  function displayRecipe(recipe, recipeElement) {
+    // Finds the first "img" element inside recipeElement and sets the src to the value of strMealThumb property of the recipe object
+    recipeElement.querySelector("img").src = recipe.strMealThumb;
+    // Finds the first "title" element inside recipeElement and sets the text content to the value of strMeal property of the recipe object
+    recipeElement.querySelector(".title").textContent = recipe.strMeal;
+    // Finds the first "p" element with the class content in recipeElement and sets the text content to the value of strInstructions property of the recipe object
+    recipeElement.querySelector(".content p").textContent = recipe.strInstructions;
+
+    // Assigns ingredients and empty array
+    var ingredients = [];
+    // For loop that will run 20 times because the API returns up to 20 ingredients
+    for (let j = 1; j <= 20; j++) {
+      // Assign the "j" ingredient and measure from the recipe object
+      var ingredient = recipe[`strIngredient${j}`];
+      var measure = recipe[`strMeasure${j}`];
+      // Checks to see if both the ingredient and measure are not null or empty. If they're good, push a string containing the ingredient and its measure into the ingredients array
+      if (ingredient && ingredient !== "" && measure && measure !== "") {
+        ingredients.push(`${ingredient} (${measure})`);
+      }
+    }
+    // Finds first "p" element with class content in recipeElement and sets text content to the ingredients array joined as a string with each ingredient separated by a comma
+    recipeElement.querySelector(".content p").textContent = ingredients.join(", ");
+    // Links user to the recipe-details.html
+    recipeElement.querySelector("a").href = `recipe-details.html`;
+    // When the link is clicked, the function is executed which stores the recipe object in the browser's localStorage as a string.
+    recipeElement.querySelector("a").addEventListener("click", function () {
+      localStorage.setItem("recipe", JSON.stringify(recipe));
+    });
+  }
   
-  
-  
+  // Calls getTopSearches function
+  getTopSearches();
