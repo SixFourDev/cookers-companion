78bdd97f32a8eca0fb56974575f7aeacee08dcfb
JoaquinGodina0 <godinajoaquin6@gmail.com>
2023-05-16 18:58:22 -0700

    deleted old code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,3 @@
-// Creates variables for our Edamam ID & Key
-var edamamAppId = '5c0067e3';
-var edamamAppKey = '56c52d9c111e1bbde1de87206bc6f1de';
-// Creates variables for our Nutritionix ID & Key
-var nutritionixAppId = '13554c1a';
-var nutritionixAppKey = '625ffc981e7b96d9c8f9946a57bd5bc8';
-// Creates variable query to top searches
-var query = "top searches";
-
 // Selects all of the card elements in each column
 var recipeElements = [
   document.querySelector(".column:nth-of-type(1) .card"),
